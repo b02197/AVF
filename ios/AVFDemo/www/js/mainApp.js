d84b95466efc2b5607b13ca6405e066769712c0b
Micheal Eaton <Meaton@fullsail.edu>
2013-07-18 13:11:27 -0400

    Add a weather api to the ios app

--- ios/AVFDemo/www/js/mainApp.js
+++ ios/AVFDemo/www/js/mainApp.js
@@ -2,7 +2,7 @@
 //AVF 1307
 //week 1 
 //AVF Demo app
-//$('#instaApi').on('pageinit', function(){
+$('#instaApi').on('pageinit', function(){
 $('#game').click(function(){
 	$(function(){
 	  	  var url = "https://api.instagram.com/v1/tags/game/media/recent?callback=?&amp;client_id=a7a95df91793454f90ae6e0602ff6123&amp;min_id=10"
@@ -40,18 +40,112 @@ $('#food').click(function(){
 	           });
 	};
 
-//})
-$(function(){
-var wUrl = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=bridgeport%2C+michigan&format=json&num_of_days=5&callback=%3F&key=up7jzu6qf3n9jp5uqsxqe9f2"
-$.getJSON(wUrl, screenOut);
 })
+/*$(function(){
+var wUrl = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
+$.getJSON(wUrl, outPutScreen);
+console.log(wUrl)
+});
 
 
-var screenOut = function(data){
+var outPutScreen = function(data){
+alert("loaded");
 	console.log(data);
-	
-	$.each(data.data,function(index, weather){
-		var weath = "<li><img src='"+ weather.weatherIconURl.value+"'</li>"
+	var ob = data.response.ob;
+	$.each(data.response,function(){
+		var weath = "<li> The weather outside is '" + ob.weather + "'</li><li>The temp is:" + ob.tempF + "<li>It feels like: " + ob.feelslikeF + "</li><li>Sun Rise it at: "+ ob.sunriseISO + "</li><li>The sun will set at: "+ ob.sunsetISO +"</li>"
 		$("#weatherLi").append(weath);
 	});
-};
+};*/
+$('#temp').click(function(){
+                 $(function(){
+                   var url = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
+                   $.getJSON(url, screenTemp);
+                   
+                   });
+                 })
+
+var screenTemp = function(data){
+	console.log(data)
+    var ob = data.response.ob;
+    
+    $.each(data.response, function(index, sun){
+           var temp = "<li> The current temperature is: "+ ob.tempF + " F</li>"
+           $('#weatherList').append(temp)
+           })
+}
+
+$('#cloud').click(function(){
+                  $(function(){
+                    var url = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
+                    $.getJSON(url, screencloud);
+                    
+                    });
+                  })
+
+var screencloud = function(data){
+	console.log(data)
+    var ob = data.response.ob;
+    
+    $.each(data.response, function(index, sun){
+           var weather = "<li> The current weather is: "+ ob.weather + "</li>"
+           $('#weatherList').append(weather)
+           })
+}
+
+$('#wind').click(function(){
+                 $(function(){
+                   var url = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
+                   var name = "food";
+                   $.getJSON(url, screenWind);
+                   
+                   });
+                 })
+
+var screenWind = function(data){
+	console.log(data)
+    var ob = data.response.ob;
+    
+    $.each(data.response, function(index, sun){
+           var winds = "<li> The current wind speed is: "+ ob.windMPH + " MPH</li>"
+           $('#weatherList').append(winds)
+           })
+}
+
+$('#feel').click(function(){
+                 $(function(){
+                   var url = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
+                   var name = "food";
+                   $.getJSON(url, screenFeel);
+                   
+                   });
+                 })
+
+var screenFeel = function(data){
+	console.log(data)
+    var ob = data.response.ob;
+    
+    $.each(data.response, function(index, sun){
+           var feels = "<li> It feels like: "+ ob.feelslikeF + " F outside.</li>"
+           $('#weatherList').append(feels)
+           })
+}
+
+$('#hum').click(function(){
+                $(function(){
+                  var url = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
+                  var name = "food";
+                  $.getJSON(url, screenhum);
+                  
+                  });
+                })
+
+var screenhum = function(data){
+	console.log(data)
+    var ob = data.response.ob;
+    
+    $.each(data.response, function(index, sun){
+           var hums = "<li> The current humiditiy is: "+ ob.humidity + "</li>"
+           $('#weatherList').append(hums)
+           })
+}
