2380f233f00b7a165bc1231b6c06dd95f7b49850
Micheal Eaton <Meaton@fullsail.edu>
2013-07-24 09:51:58 -0400

    Change html tags so im not using to my section tags. Change what is going into the app as well. IOS.

--- ios/AVFDemo/www/js/mainApp.js
+++ ios/AVFDemo/www/js/mainApp.js
@@ -2,6 +2,10 @@
 //AVF 1307
 //week 1 
 //AVF Demo app
+
+
+//Functions for instagram
+
 $('#instaApi').on('pageinit', function(){
 $('#game').click(function(){
 	$(function(){
@@ -41,120 +45,13 @@ $('#food').click(function(){
 	};
 
 })
-/*$(function(){
-var wUrl = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
-$.getJSON(wUrl, outPutScreen);
-console.log(wUrl)
-});
 
-
-var outPutScreen = function(data){
-alert("loaded");
-	console.log(data);
-	var ob = data.response.ob;
-	$.each(data.response,function(){
-		var weath = "<li> The weather outside is '" + ob.weather + "'</li><li>The temp is:" + ob.tempF + "<li>It feels like: " + ob.feelslikeF + "</li><li>Sun Rise it at: "+ ob.sunriseISO + "</li><li>The sun will set at: "+ ob.sunsetISO +"</li>"
-		$("#weatherLi").append(weath);
-	});
-};*/
-/*$('#temp').click(function(){
-                 $(function(){
-                   var url = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
-                   $.getJSON(url, screenTemp);
-                   
-                   });
-                 })
-
-var screenTemp = function(data){
-	console.log(data)
-    var ob = data.response.ob;
-    
-    $.each(data.response, function(index, sun){
-           var temp = "<li> The current temperature is: "+ ob.tempF + " F</li>"
-           $('#weatherList').append(temp)
-           })
-}
-
-$('#cloud').click(function(){
-                  $(function(){
-                    var url = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
-                    $.getJSON(url, screencloud);
-                    
-                    });
-                  })
-
-var screencloud = function(data){
-	console.log(data)
-    var ob = data.response.ob;
-    
-    $.each(data.response, function(index, sun){
-           var weather = "<li> The current weather is: "+ ob.weather + "</li>"
-           $('#weatherList').append(weather)
-           })
-}
-
-$('#wind').click(function(){
-                 $(function(){
-                   var url = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
-                   var name = "food";
-                   $.getJSON(url, screenWind);
-                   
-                   });
-                 })
-
-var screenWind = function(data){
-	console.log(data)
-    var ob = data.response.ob;
-    
-    $.each(data.response, function(index, sun){
-           var winds = "<li> The current wind speed is: "+ ob.windMPH + " MPH</li>"
-           $('#weatherList').append(winds)
-           })
-}
-
-$('#feel').click(function(){
-                 $(function(){
-                   var url = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
-                   var name = "food";
-                   $.getJSON(url, screenFeel);
-                   
-                   });
-                 })
-
-var screenFeel = function(data){
-	console.log(data)
-    var ob = data.response.ob;
-    
-    $.each(data.response, function(index, sun){
-           var feels = "<li> It feels like: "+ ob.feelslikeF + " F outside.</li>"
-           $('#weatherList').append(feels)
-           })
-}
-
-$('#hum').click(function(){
-                $(function(){
-                  var url = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
-                  var name = "food";
-                  $.getJSON(url, screenhum);
-                  
-                  });
-                })
-
-var screenhum = function(data){
-	console.log(data)
-    var ob = data.response.ob;
-    
-    $.each(data.response, function(index, sun){
-           var hums = "<li> The current humiditiy is: "+ ob.humidity + "</li>"
-           $('#weatherList').append(hums)
-           })
-}*/
-
-
+//Functions for Weather api
+//
 $(function(){
-  var wUrl = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH"
+  var wUrl = "http://api.aerisapi.com/observations/bridgeport,mi?client_id=4J0DvsCTHRMqeVoRsv1mz&client_secret=uwQuCX7jTQ2XaRxgxa3zc88E1AYAZouq3gXlggoH";
   $.getJSON(wUrl, outPutScreen);
-  console.log(wUrl)
+  console.log('load weather');
   });
 
 
@@ -168,3 +65,4 @@ var outPutScreen = function(data){
 	$('#weatherList').append(wind);
 	$('#weatherList').append(weather);
 };
+
